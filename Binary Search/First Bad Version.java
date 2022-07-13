cbba9b9cecf321aaae5b1e937db1c811ed4d7b90
cfutt36592 <39533354+cfutt36592@users.noreply.github.com>
2022-07-13 08:50:23 -0400

    Create First Bad Version.java

---
+++ Binary Search/First Bad Version.java
@@ -0,0 +1,27 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if(n == 1) return n;
+        
+        int left = 0;
+        int right = n;
+        
+        while(left<=right){
+            int mid = left + (right-left) / 2;
+            if(isBadVersion(mid) && !isBadVersion(mid-1)){
+                return mid;
+            }
+            else if(!isBadVersion(mid)){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        
+        return -1;
+    }
+    
+}
